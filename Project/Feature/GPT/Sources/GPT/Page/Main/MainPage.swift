56f99c981e9674fd15557e35be866ae05f9b183a
seungchan2022 <110214307+seungchan2022@users.noreply.github.com>
2023-10-08 22:38:17 +0900

    feat: ScrollViewReader추가(데이터 받는 도중 자동 스크롤), TextField 수정

--- Project/Feature/GPT/Sources/GPT/Page/Main/MainPage.swift
+++ Project/Feature/GPT/Sources/GPT/Page/Main/MainPage.swift
@@ -5,21 +5,22 @@ import SwiftUI
 // MARK: - MainPage
 
 struct MainPage {
-
+  
   init(store: StoreOf<MainStore>) {
     self.store = store
     viewStore = ViewStore(store, observe: { $0 })
   }
-
+  
   let store: StoreOf<MainStore>
   @ObservedObject private var viewStore: ViewStoreOf<MainStore>
+  @Namespace private var lastMessage
 }
 
 extension MainPage {
   private var isLoading: Bool {
     viewStore.fetchMessage.isLoading
   }
-
+  
   private var message: String {
     viewStore.fetchMessage.value.content
   }
@@ -31,40 +32,51 @@ extension MainPage: View {
   var body: some View {
     VStack {
       Spacer()
-
+      
       Text("GPT에게 말해봐요")
-
-      ScrollView {
-        Text(message)
-
-        if isLoading {
+      
+      ScrollViewReader { proxy in
+        ScrollView {
+          Text(message)
+          
           ProgressView()
             .progressViewStyle(CircularProgressViewStyle())
+            .opacity(isLoading ? 1 : .zero)
+            .id(lastMessage)
+        }
+        .onChange(of: message) { _, _ in
+          proxy.scrollTo(lastMessage, anchor: .bottom)
         }
       }
-
+      
       Spacer()
       switch isLoading {
       case true:
-        Text("출력중....")
-
-      case false:
         HStack {
-          TextField("여기서 입력하세요", text: viewStore.$message)
-            .frame(maxWidth: .infinity)
-            .textFieldStyle(RoundedBorderTextFieldStyle())
-            .disabled(isLoading)
+          Text("출력중.....")
+          Button(action: { viewStore.send(.onTapCancel)}) {
+            Text("중단")
+          }
+        }
+        
+      case false:
+        HStack(alignment: .bottom) {
+          TextField("", text: viewStore.$message, prompt: Text("여기에 입력하세요"), axis: .vertical)
+            .frame(minHeight: 50)
             .onSubmit {
-              viewStore.send(.sendMessage)
+              viewStore.send(.onTapSendMessage)
             }
-
-          Button(action: { viewStore.send(.sendMessage) }, label: {
+          
+          Button(action: { viewStore.send(.onTapSendMessage) }) {
             Text("전송")
-          })
+              .frame(minHeight: 50)
+          }
         }
         .disabled(isLoading)
-        .padding(.vertical, 20)
         .padding(.horizontal, 16)
+        .border(Color.blue)
+        .frame(maxHeight: 120)
+        
       }
     }
     .padding(.horizontal, 16)
